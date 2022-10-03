6135f4cd2e0ba8bea53d0459af869871521f8098
Genaro08 <71444929+Genaro08@users.noreply.github.com>
2022-10-03 19:12:22 -0300

    Add files via upload

---
+++ listadoClientes.php
@@ -0,0 +1,28 @@
+<h1>Listado Clientes</h1>
+	<form action="AdministradorModificaCliente.php" method="POST">
+		<label for="Clientes">Clientes: </label>
+		<select id="Clientes" name="Clientes" >
+			<?php
+				require 'conexion.php';
+				session_start();
+
+				$consulta = "SELECT nombre, apellido, DNI FROM Cliente";
+				
+				$resultado = mysqli_query($conexion,$consulta);
+				if ($resultado) {
+					while ($row = $resultado->fetch_array()) {
+						$nombre = $row['nombre'];
+						$apellido = $row['apellido'];
+						$dni =  $row['DNI'];
+			?>
+						<option value = <?php echo $dni;?> ><?php echo $nombre; echo " $apellido";?></option>
+			<?php
+					}
+				}
+			?>
+		</select required><br>
+		<p></p> 	
+		<button> Modificar Datos Basicos </button><br>	
+	</form>
+
+		<button onclick="location.href='PantallaAdministrador.html'"> Cancelar </button>
