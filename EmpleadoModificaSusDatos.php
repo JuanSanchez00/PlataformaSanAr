0b375f0cf02801be4511156e0dde5a59cb52c539
Juan <juanignacios.2000@gmail.com>
2022-10-04 16:50:00 -0300

    cambios en empleado

--- EmpleadoModificaSusDatos.php
+++ EmpleadoModificaSusDatos.php
@@ -1,57 +1,87 @@
 <html>
 	<head>
-		<title>Modificar Datos Administrador</title>
+		<title>Modificar Datos Empleado</title>
 	</head>
 	<body>
 		<h1>Modificar datos basicos</h1>
 
-       	<form>
+		<?php
+			require 'conexion.php';
+			session_start();
+			$empleado=$_GET["empleado"];
+			$consulta = "SELECT nombre, apellido, fecha_nac, DNI, email, provincia, localidad, calle, depto, CP, tel, rol, sucursal FROM Empleado 
+			WHERE DNI = ".$empleado.";";
+
+			$resultado = mysqli_query($conexion,$consulta);
+
+			if ($resultado){
+				while ($row = $resultado->fetch_array()) {
+					$Nombre = $row['nombre'];
+					$Apellido = $row['apellido'];
+					$Fecha = $row['fecha_nac'];
+					$Dni = $row['DNI'];
+					$Email = $row['email'];
+					$Provincia = $row['provincia'];
+					$Localidad = $row['localidad'];
+					$Calle = $row['calle'];
+					$Depto = $row['depto'];
+					$Cp = $row['CP'];
+					$Tel = $row['tel'];
+					$Rol = $row['rol'];
+					$Sucursal = $row['sucursal'];
+				}
+			}	
+		?>
+
+
+       	<form action= "modificarClienteDesdeCliente.php" method="POST">
        		<p>Los campos marcados con * son obligatorios</p>
 
        		<label for="Nombre">* Nombre: </label><br>
-			<input type="text" id="Nombre" name="Nombre" required><br>
+			<input type="text" id="Nombre" name="Nombre" value = <?php echo $Nombre;?> required><br>
 
 			<label for="Apellido">* Apellido: </label><br>
-			<input type="text" id="Apellido" name="Apellido" required><br>
+			<input type="text" id="Apellido" name="Apellido" value = <?php echo $Apellido;?> required><br>
 
 			<label for="FechaDeNacimiento">* Fecha de Nacimiento: </label><br>
-			<input type="date" id="FechaDeNacimiento" name="FechaDeNacimiento" required><br>
+			<input type="date" id="FechaDeNacimiento" name="FechaDeNacimiento" value = <?php echo $Fecha;?>required><br>
 
 			<label for="DNI">* DNI: </label><br>
-			<input type="number" id="DNI" name="DNI" min="0" onkeypress="return SoloEnteroPositivo(event);"ondrop="return false;" onpaste="return false;" required><br>
+			<input type="number" id="DNI" name="DNI" min="0" onkeypress="return SoloEnteroPositivo(event);"ondrop="return false;" onpaste="return false;" value = <?php echo $Dni;?> required><br>
 
 			<label for="Email">* Email: </label><br>
-			<input type="text" id="Email" name="Email" required><br>
+			<input type="text" id="Email" name="Email" value = <?php echo $Email;?> required><br>
 
 			<label for="Provincia">* Provincia: </label><br>
-			<select>
+			<select name="Provincia">
 				<option value="BuenosAires" selected>Buenos Aires</option>
  				<option value="Mendoza">Mendoza</option>
 			</select id="Provincia" name="Provincia" required><br>
 
 			<label for="Localidad">* Localidad: </label><br>
-			<select>
+			<select name="Localidad">
 				<option value="BahiaBlanca" selected>Bahía Blanca</option>
  				<option value="Mendoza">Mendoza</option>
 			</select id="Localidad" name="Localidad" required><br>
 
 			<label for="Calle">* Calle: </label><br>
-			<input type="text" id="Calle" name="Calle" required><br>
+			<input type="text" id="Calle" name="Calle" value = <?php echo $Calle;?> required><br>
 
 			<label for="DeptoCasa">Depto o casa nº: </label><br>
-			<input type="text" id="DeptoCasa" name="DeptoCasa" ><br>
+			<input type="text" id="DeptoCasa" name="DeptoCasa" value = <?php echo $Depto;?>><br>
 
 			<label for="CP">* CP: </label><br>
-			<input type="number" id="CP" name="CP" min="0" onkeypress="return SoloEnteroPositivo(event);"ondrop="return false;" onpaste="return false;" required><br>
+			<input type="number" id="CP" name="CP" min="0" onkeypress="return SoloEnteroPositivo(event);"ondrop="return false;" onpaste="return false;"  value = <?php echo $Cp;?> required><br>
 
 			<label for="Telefono">* Teléfono: </label><br>
-			<input type="number" id="Telefono" name="Telefono" min="0" onkeypress="return SoloEnteroPositivo(event);"ondrop="return false;" onpaste="return false;" required><br>
+			<input type="number" id="Telefono" name="Telefono" min="0" onkeypress="return SoloEnteroPositivo(event);"ondrop="return false;" onpaste="return false;" value = <?php echo $Tel;?> required><br>
 
-			<label for="Rol">* Rol: </label><br>
-			<select>
-				<option value="Rol1" selected>Rol1</option>
- 				<option value="Rol2">Rol2</option>
-			</select id="Rol" name="Rol" required><br>
+			<label for="Plan" name="Plan">* Plan: </label><br>
+			<select name="Plan">
+				<?php
+					include("listadoPlanes.php")
+				?>
+			</select id="Plan" required><br>
 
        		<script type="text/javascript">
 			    function SoloEnteroPositivo(e) {
@@ -68,14 +98,11 @@
 			    }
 			</script>
 
-			<br><button onclick="location.href='PantallaEmpleado.html'"> Cancelar </button> <input type="submit" value="Confirmar">
+			<button for="Confirmar" name = "Cliente" value = <?php echo $cliente;?>> Confirmar </button><br>
 
        	</form>
-
+       	<button onclick="location.href='PantallaCliente.php?cliente=<?php echo $cliente?>'"> Cancelar </button>
 
 
     </body>
-
-
-
-</html>
+   </html>
