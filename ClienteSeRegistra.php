b5a385c3ae12f644498ec27432498c2dd74fefae
Juan <juanignacios.2000@gmail.com>
2022-10-04 18:06:31 -0300

    control en el dni del cliente cuando el mismo se registra

--- ClienteSeRegistra.php
+++ ClienteSeRegistra.php
@@ -18,13 +18,25 @@
 	$codigo = $_POST['Codigo'];
 	$plan = $_POST['Plan'];
 
+	$consulta = "SELECT EXISTS (SELECT * FROM Cliente WHERE DNI='$dni')";
 
-	$consulta = "INSERT INTO Cliente VALUES ('".$nombre."', '".$apellido."', ".$fecha.", ".$dni.", '".$password."', '".$email."', '".$provincia."', '".$localidad."', '".$calle." ".$numero."', '".$depto."', ".$cp.", ".$telefono.", '".$plan."')";
+	$resultado = mysqli_query($conexion,$consulta);
+	$array = mysqli_fetch_array($resultado);
+
+	if($array[0]=="1"){
+		echo "<script> alert('DNI ya registrado en el sistema. Por favor ingrese otro.');  window.location='ClienteRegistraACliente.php'; </script>";
+	}
+	else {
+		$consulta = "INSERT INTO Cliente VALUES ('".$nombre."', '".$apellido."', ".$fecha.", ".$dni.", '".$password."', '".$email."', '".$provincia."', '".$localidad."', '".$calle." ".$numero."', '".$depto."', ".$cp.", ".$telefono.", '".$plan."')";
 
 	$resultado = mysqli_query($conexion,$consulta);
 	
 	if ($resultado) {
 		echo "<script> alert('Usuario creado correctamente.');  window.location='LoginCliente.php'; </script>";
 	}
+	}
+
+
+	
 
 	?>
