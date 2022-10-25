9aaf5531ea2d1ad3c7b1f6ee08390c8784754529
Thiago Zanconi <thiagzanc@gmail.com>
2022-10-25 19:10:26 -0300

    Update ClienteGeneraCuponDePago.php

--- ClienteGeneraCuponDePago.php
+++ ClienteGeneraCuponDePago.php
@@ -20,6 +20,26 @@
 					$Plan = $row['plan'];
 				}
 			}	
+
+			$cliente_menor=$_GET["cliente_menor"];
+			$consulta2 = "SELECT Cliente_menor.nombre, Cliente_menor.apellido, Cliente_menor.DNI FROM Cliente INNER JOIN Cliente_menor ON Cliente.DNI=Cliente_menor.DNI_tutor";
+
+			$resultado2 = mysqli_query($conexion,$consulta2);
+
+			if($resultado2){
+				while($row = $resultado2->fetch_array()){
+					$NombreMenor = $row['nombre'];
+					$ApellidoMenor = $row['apellido'];
+					$DniMenor = $row['DNI'];
+				}
+			}
+			else{
+				$NombreMenor = "";
+				$ApellidoMenor = "";
+				$DniMenor = "";
+			}
+
+
 		?>
 		<label for="Nombre">Nombre: </label>
 		<label for="ValNombre"><?php echo $Nombre;?></label><br>
@@ -31,11 +51,24 @@
 		<label for="ValDni"><?php echo $Dni;?></label><br>
 
 		<label for="Menor a cargo">Menor a cargo: </label><br>
+
+		<label for="	NombreMenor">Nombre: </label>
+		<label for="ValNombreMenor"><?php echo $NombreMenor;?></label><br>
+
+		<label for="	ApellidoMenor">Apellido: </label>
+		<label for="ValApellidoMenor"><?php echo $ApellidoMenor;?></label><br>
+
+		<label for="	DNIMenor">DNI: </label>
+		<label for="ValDniMenor"><?php echo $DniMenor;?></label><br>
+
+		<label for="	MontoMenor">Monto: </label>
+
+
 		<label for="Fecha de vencimiento">Fecha de vencimiento: </label><br>
 
 		<label for="Plan">Plan: </label>
 		<label for="ValPlan"><?php echo $Plan;?></label><br>
-		
+
 		<label for="Cuota">Cuota: </label><br>
 		<label for="Monto">Monto: </label><br>
 
