22c35af4fbd16191a0ebbe7484eb9f7664424d5a
Genaro08 <71444929+Genaro08@users.noreply.github.com>
2022-10-27 13:27:24 -0300

    US 33 con el boton

---
+++ EmpleadoListaSolicitudes.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+		<title>Listado Solicitudes</title>
+	</head>
+
+	<body>
+		<center>Solicitudes</center>
+		<table>
+	  		<tbody>
+    			<tr>
+      				<th>ID</th>
+      				<th>Tipo</th>
+      				<th>Apellido</th>
+      				<th>Nombre</th>
+      				<th>DNI</th>
+      				<th>Mas Info</th>
+      				<th>Estado</th>
+    			</tr>
+    			<?php
+					$empleado = $_GET['empleado'];
+          include("mostrarSolicitudesDesdeEmpleado.php")
+				?>
+  			</tbody>
+		</table>
+    <br>
+    <button onclick="location.href='PantallaEmpleado.php?empleado=<?php echo $empleado?>'"> Volver </button> 
+	<body>
+
+</html>
