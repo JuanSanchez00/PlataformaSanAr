62015c13f057f32695129a8f1f807b0fd82b5b03
Thiago Zanconi <thiagzanc@gmail.com>
2022-10-28 16:26:29 -0300

    Update ClienteGeneraCuponDePago.php

--- ClienteGeneraCuponDePago.php
+++ ClienteGeneraCuponDePago.php
@@ -3,8 +3,16 @@
 		<h1>Generar Cupon de Pago</h1>
 	</head>
 	<body>
-		<button onclick="location.href='GenerarCuponDePago.php'"> Generar cupon de pago semestral </button><br>
-		<button onclick="location.href='GenerarCuponDePago.php'"> Generar cupon de pago anual </button><br>
+		<?php $cliente=$_GET["cliente"];?>
+		
+		<select onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
+		    <option value="" selected disabled>Operaciones</option>
+			<option value="GenerarCuponDePago.php<?php echo"?cliente=$cliente"?>">Generar cupon de pago mensual</option>
+			<option value="GenerarCuponDePago.php<?php echo"?cliente=$cliente"?>">Generar cupon de pago semestral</option>
+			<option value="GenerarCuponDePago.php<?php echo"?cliente=$cliente"?>">Generar cupon de pago anual</option>
+		</select>
+		
+		<br>
 		<button onclick="location.href='PantallaCliente.php'"> Volver </button>
 	</body>
 </html>
